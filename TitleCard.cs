189f3c5d5c81ce789697fee732191a650b7c198c
BottleGnomes <LineForHire@gmail.com>
2015-05-08 01:54:19 -0400

    titleTiming

--- TitleCard.cs
+++ TitleCard.cs
@@ -14,7 +14,7 @@ namespace RogueLikeGame
         RogueLike ingame;
         Texture2D overlay;
         SpriteFont output128;
-        Vector2 textLocation = new Vector2(384, -300);
+        Vector2 textLocation = new Vector2(384, 100);
         KeyboardState state;
         int titleTimer = 0;
         public TitleCard(SpriteBatch spriteBatch, RogueLike ingame)
@@ -34,7 +34,7 @@ namespace RogueLikeGame
             if (textLocation.Y < 360) { textLocation.Y += (float)(.008) * (390 - textLocation.Y); }
             if (textLocation.Y > 360) { textLocation.Y = 360; }
             if (state.IsKeyDown(Keys.Escape)) { ingame.changeGameState("playing"); }
-            else if (titleTimer >= 6400) { ingame.changeGameState("playing"); }
+            else if (titleTimer >= 4000) { ingame.changeGameState("playing"); }
         }
         public void draw()
         {
